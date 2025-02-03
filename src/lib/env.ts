c9245967d1c000106dfe84569dbc2e18c444f841
Eirik Kirkeli <115043925+Knirkeli@users.noreply.github.com>
2025-02-03 11:36:32 +0100

    Første oppstart

--- src/lib/env.ts
+++
@@ -1,3 +0,0 @@
-export const dev = process.env.NODE_ENV === 'development'
-
-export const vercelPreview = process.env.VERCEL_ENV === 'preview'
--- src/lib/moduleProps.ts
+++
@@ -1,9 +0,0 @@
-import { stegaClean } from 'next-sanity'
-
-export default function ({ _type, options, _key }: Partial<Sanity.Module>) {
-	return {
-		id: stegaClean(options?.uid) || 'module-' + _key,
-		'data-module': _type,
-		hidden: options?.hidden,
-	}
-}
